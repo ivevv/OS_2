d84a40055623fe9addbe75dbcc3c482589412312
eugenia <55488624+ivevv@users.noreply.github.com>
2021-03-03 20:07:25 +0300

    Add files via upload

---
+++ lab2_1.cpp
@@ -0,0 +1,73 @@
+//Иванчук Евгения, 9894
+
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <semaphore.h>
+
+using namespace std;
+
+sem_t sem;
+bool flag = true;
+
+void* func1(void *arg)
+{
+	int *npt = (int*) arg;
+	int n = *npt;
+	while (flag == 1)
+	{
+		sem_wait(&sem);
+		for (int i = 0; i < n; i++)
+		{
+			if (flag != 1)
+			break;
+			cout << "1";
+			cout.flush();
+			sleep(1);
+		}
+		sem_post(&sem);
+		sleep(1);
+	}
+	return NULL;
+}
+
+void* func2(void *arg)
+{
+	int *npt = (int*) arg;
+	int n = *npt;
+	while (flag == 1)
+	{
+		sem_wait(&sem);
+		for (int i = 0; i < n; i++)
+		{
+			if (flag != 1)
+			break;
+			cout << "2";
+			cout.flush();
+			sleep(1);
+		}
+	sem_post(&sem);
+	sleep(1);
+}
+	return NULL;
+}
+
+int main()
+{
+	sem_init(&sem, 0, 1);
+	int arg;
+	cout << "Введите число повторений:" << endl;
+	cin >> arg;
+	cin.clear();
+	pthread_t th1, th2;
+	pthread_create(&th1, NULL, func1, (void*) &arg);
+	pthread_create(&th2, NULL, func2, (void*) &arg);
+	cin.ignore();
+	getchar();
+	flag = false;
+	pthread_join(th1, NULL);
+	pthread_join(th2, NULL);
+	sem_destroy(&sem);
+	return 0;
+}
---
+++ lab2_3.cpp
@@ -0,0 +1,93 @@
+//Иванчук Евгения, 9894
+
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <semaphore.h>
+#include <time.h>
+
+using namespace std;
+
+sem_t sem;
+bool flag = true;
+struct timespec tp;
+
+void* func1(void *arg)
+{
+	int *npt = (int*) arg;
+	int n = *npt;
+	while (flag == 1)
+	{
+		clock_gettime(CLOCK_REALTIME, &tp);
+		tp.tv_sec++;
+		int rv = sem_timedwait(&sem, &tp);
+		if (rv != 0)
+		{
+			sleep(1);
+		}
+		else
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if (flag != 1)
+				break;
+				cout << "1";
+				cout.flush();
+				sleep(1);
+			}
+			sem_post(&sem);
+			sleep(1);
+		}
+	}
+	return NULL;
+}
+
+void* func2(void *arg)
+{
+	int *npt = (int*) arg;
+	int n = *npt;
+	while (flag == 1)
+	{
+		clock_gettime(CLOCK_REALTIME, &tp);
+		tp.tv_sec++;
+		int rv = sem_timedwait(&sem, &tp);
+		if (rv !=0)
+		{
+			sleep(1);
+		}
+		else
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if (flag != 1)
+				break;
+				cout << "2";
+				cout.flush();
+				sleep(1);
+			}
+			sem_post(&sem);
+			sleep(1);
+		}
+	}
+	return NULL;
+}
+
+int main()
+{
+	sem_init(&sem, 0, 1);
+	int arg;
+	cout << "Введите число повторений:" << endl;
+	cin >> arg;
+	cin.clear();
+	pthread_t th1, th2;
+	pthread_create(&th1, NULL, func1, (void*) &arg);
+	pthread_create(&th2, NULL, func2, (void*) &arg);
+	cin.ignore();
+	getchar();
+	flag = false;
+	pthread_join(th1, NULL);
+	pthread_join(th2, NULL);
+	sem_destroy(&sem);
+	return 0;
+}
